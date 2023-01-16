01542ceeeb044c211a6c3fe52bc3529404189f6e
jaxonhill <jaxoncharleshill@gmail.com>
2023-01-16 11:39:55 -0800

    Add date formatting

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import Layout from "../../components/Layout"
 import { getAllPostIds, getPostData } from "../../lib/posts"
+import Date from "../../components/Date";
 
 export default function PostPage({ postData }) {
     return (
         <Layout home={false}>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br />
             {postData.id}
             <br />
-            {postData.date}
+            <Date dateString={postData.date} />
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     )
